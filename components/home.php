2219f4fca5cca52d3c31ce15abbad1ee49daaa00
adventurousgeek <geekspod@gmail.com>
2019-07-16 18:25:11 +0500

    Features Section Updated!

--- components/home.php
+++ components/home.php
@@ -13,8 +13,8 @@
     </div>
 </section>
 
-<section class="features-section">
-    <div class="container">
+<section class="features-images">
+<div class="container">
         <div class="row">
             <div class="col-md-12">
                 <h1 class="text-center">Features</h1>
@@ -23,21 +23,35 @@
         </div>
         <div class="row">
             <div class="col-md-4">
-                <img src="assets/images/vouching.png" class="vouching-image">
+                <img src="assets/images/vouching.png" class="vouche-image">
+            </div>
+            <div class="col-md-4">
+                <img src="assets/images/collaborate.png" class="collaborate-image">
+            </div>
+            <div class="col-md-4">
+                <img src="assets/images/investigate.png" class="investigate-image">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="features-section">
+<img src="assets/images/curves.png" class="curve-image">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
                 <h2>Vouch to Earn</h2>
                 <p>Invite your friends, vouch for them and you both get rewarded with GFT token.</p>
                 <div class="col text-center">
                 <a target="_blank" href="https://gyftieapp.org"><button type="button" class="btn btn-primary">READ MORE</button></a></div>
             </div>
             <div class="col-md-4">
-                <img src="assets/images/collaborate.png" class="collaborate-image">
                 <h2>Propose & Collaborate</h2>
                 <p>Earn more when you participate. Everyone has a vote and can even propose changes.</p>
                 <div class="col text-center">
                 <a target="_blank" href="https://gyftieapp.org"><button type="button" class="btn btn-primary">READ MORE</button></a></div>
             </div>
             <div class="col-md-4">
-                <img src="assets/images/investigate.png" class="investigate-image">
                 <h2>Investigate</h2>
                 <p>If someone cheats the system, we close the account and share ½ the coins with the Gyftie Community and ½ with the investigator.</p>
                 <div class="col text-center">
