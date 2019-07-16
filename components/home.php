4e5b4eba43d1d4e9112552a0fc3fce1c0dc9d15b
adventurousgeek <geekspod@gmail.com>
2019-07-16 17:31:44 +0500

    Buttons Added!

--- components/home.php
+++ components/home.php
@@ -4,7 +4,7 @@
             <div class="col-md-5">
                 <h1>Vouching for a better world</h1>
                 <p>Gyftie is a financial reputation system designed with collaborative intelligence to help empower the world.  We align personal integrity with financial self-interest in a manner that accelerates growth, reduces fraud, and reduces poverty. And it all starts with a simple gift (GFT) from one friend to another.</p>
-                <a href="https://gyftieapp.org"><button type="button" class="btn btn-primary">OPEN WEB APP</button></a><a href="https://play.google.com/store/apps/details?id=com.gyftie&hl=en"><img src="assets/images/google-play-icon.png"></a><img src="assets/images/app-store-icon.png">
+                <a target="_blank" href="https://gyftieapp.org"><button type="button" class="btn btn-primary">OPEN WEB APP</button></a><a target="_blank" href="https://play.google.com/store/apps/details?id=com.gyftie&hl=en"><img src="assets/images/google-play-icon.png"></a><img src="assets/images/app-store-icon.png">
             </div>
             <div class="col-md-7">
                 
@@ -26,16 +26,22 @@
                 <img src="assets/images/vouching.png" class="vouching-image">
                 <h2>Vouch to Earn</h2>
                 <p>Invite your friends, vouch for them and you both get rewarded with GFT token.</p>
+                <div class="col text-center">
+                <a target="_blank" href="https://gyftieapp.org"><button type="button" class="btn btn-primary">READ MORE</button></a></div>
             </div>
             <div class="col-md-4">
                 <img src="assets/images/collaborate.png" class="collaborate-image">
                 <h2>Propose & Collaborate</h2>
                 <p>Earn more when you participate. Everyone has a vote and can even propose changes.</p>
+                <div class="col text-center">
+                <a target="_blank" href="https://gyftieapp.org"><button type="button" class="btn btn-primary">READ MORE</button></a></div>
             </div>
             <div class="col-md-4">
                 <img src="assets/images/investigate.png" class="investigate-image">
                 <h2>Investigate</h2>
                 <p>If someone cheats the system, we close the account and share ½ the coins with the Gyftie Community and ½ with the investigator.</p>
+                <div class="col text-center">
+                <a target="_blank" href="https://gyftieapp.org"><button type="button" class="btn btn-primary">READ MORE</button></a></div>
             </div>
         </div>
     </div>
@@ -53,8 +59,8 @@
             </div>
             <div class="col-md-6">
                 <h2>Join the hive</h2>
-                <p>The Gyftie Hypothesis is that collaborative-intelligent-interaction and real-time-adaptability is sufficient to address & adapt to the various issues as they arise. 
-Help us to improve and evolve Gyftie by collaboration and proposals.</p>
+                <p>The Gyftie Hypothesis is that collaborative-intelligent-interaction and real-time-adaptability is sufficient to address & adapt to the various issues as they arise. Help us to improve and evolve Gyftie by collaboration and proposals.</p>
+                <a target="_blank" href="https://gyftieapp.org"><button type="button" class="btn btn-primary">READ MORE</button></a>
             </div>
         </div>
     </div>
